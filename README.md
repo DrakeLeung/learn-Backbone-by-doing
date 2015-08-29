ddaaeba02fe8e92ca4b318ad7f94f392dfd01c48
DrakeLeung <drakeleung42@gmail.com>
2015-08-29 13:22:21 +0800

    update webpack.config & package.json

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # learn-Backbone-by-doing
 learn Backbone by doing some demos
 
+## Usage
+- Install the dependencies: `npm install`
+- Run the webpack-dev-server: `npm run dev`
+
 ## License
 MIT
--- webpack.config.js
+++ webpack.config.js
@@ -10,20 +10,46 @@ module.exports = {
 
 	output: {
 		path: __dirname,
-		filename: 'bundle.js'
+		filename: 'bundle.js',
+		//publicPath: 'http://localhost:3000'
 	},
 
 	module: {
 		loaders: [{
+			test: /\.js$/,
 			exclude: /node_modules\//,
 			loader: 'babel-loader'
+		}, {
+			test: /\.css$/,
+			loader: 'style!css'
+		}, {
+			test: /\.html/,
+			loader: 'html'
 		}]
+	},
+
+	resolve: {
+		//root: [],
+
+		extensions: [
+			'', '.js', '.html', '.css'
+		],
+		//moduleDirectories: [
+		//	__dirname + '/app',
+		//	'node_modules'
+		//],
+
+		alias: {
+			'underscore': 'backbone/node_modules/underscore'
+		}
 	},
 
 	plugins: [
 		new webpack.HotModuleReplacementPlugin(),
 		new webpack.NoErrorsPlugin()
 	],
+
+	devtool : 'source-map',
 
 	devServer: {
 		noInfo: false,
